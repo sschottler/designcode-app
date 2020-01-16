c8d5bf29611054a5dc1e3a9f318e18665ef558fd
Scott Schottler <scott.schottler@rallyhealth.com>
2020-01-16 16:08:55 -0500

    added contentful graphql

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -15,6 +15,40 @@ import Course from "../components/Course";
 import Menu from "../components/Menu";
 import { connect } from "react-redux";
 import Avatar from "../components/Avatar";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+
+const CardsQuery = gql`
+  {
+    cardsCollection {
+      items {
+        title
+        subtitle
+        image {
+          title
+          description
+          contentType
+          fileName
+          size
+          url
+          width
+          height
+        }
+        caption
+        logo {
+          title
+          description
+          contentType
+          fileName
+          size
+          url
+          width
+          height
+        }
+      }
+    }
+  }
+`;
 
 function mapStateToProps(state) {
   return { action: state.action, name: state.name };
@@ -120,24 +154,35 @@ class HomeScreen extends React.Component {
                 style={{ paddingBottom: 30 }}
                 showsHorizontalScrollIndicator={false}
               >
-                {cards.map((card, index) => (
-                  <TouchableOpacity
-                    key={index}
-                    onPress={() => {
-                      this.props.navigation.push("Section", {
-                        section: card
-                      });
-                    }}
-                  >
-                    <Card
-                      title={card.title}
-                      image={card.image}
-                      caption={card.caption}
-                      logo={card.logo}
-                      subtitle={card.subtitle}
-                    />
-                  </TouchableOpacity>
-                ))}
+                <Query query={CardsQuery}>
+                  {({ loading, error, data }) => {
+                    if (loading) return <Message>Loading...</Message>;
+                    if (error) return <Message>Error...</Message>;
+
+                    return (
+                      <CardsContainer>
+                        {data.cardsCollection.items.map((card, index) => (
+                          <TouchableOpacity
+                            key={index}
+                            onPress={() => {
+                              this.props.navigation.push("Section", {
+                                section: card
+                              });
+                            }}
+                          >
+                            <Card
+                              title={card.title}
+                              image={card.image}
+                              caption={card.caption}
+                              logo={card.logo}
+                              subtitle={card.subtitle}
+                            />
+                          </TouchableOpacity>
+                        ))}
+                      </CardsContainer>
+                    );
+                  }}
+                </Query>
               </ScrollView>
               <Subtitle>Popular Courses</Subtitle>
               {courses.map((course, index) => (
@@ -161,6 +206,17 @@ class HomeScreen extends React.Component {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomeScreen);
+
+const Message = styled.Text`
+  margin: 20px;
+  color: #b8bece;
+  font-size: 15px;
+  font-weight: 500;
+`;
+
+const CardsContainer = styled.View`
+  flex-direction: row;
+`;
 
 const RootView = styled.View`
   background: black;
@@ -230,36 +286,36 @@ const logos = [
   }
 ];
 
-const cards = [
-  {
-    title: "React Native for Designers",
-    image: require("../assets/background11.jpg"),
-    subtitle: "React Native",
-    caption: "1 of 12 sections",
-    logo: require("../assets/logo-react.png")
-  },
-  {
-    title: "Styled Components",
-    image: require("../assets/background12.jpg"),
-    subtitle: "React Native",
-    caption: "2 of 12 sections",
-    logo: require("../assets/logo-react.png")
-  },
-  {
-    title: "Props and Icons",
-    image: require("../assets/background13.jpg"),
-    subtitle: "React Native",
-    caption: "3 of 12 sections",
-    logo: require("../assets/logo-react.png")
-  },
-  {
-    title: "Static Data and Loop",
-    image: require("../assets/background14.jpg"),
-    subtitle: "React Native",
-    caption: "4 of 12 sections",
-    logo: require("../assets/logo-react.png")
-  }
-];
+// const cards = [
+//   {
+//     title: "React Native for Designers",
+//     image: require("../assets/background11.jpg"),
+//     subtitle: "React Native",
+//     caption: "1 of 12 sections",
+//     logo: require("../assets/logo-react.png")
+//   },
+//   {
+//     title: "Styled Components",
+//     image: require("../assets/background12.jpg"),
+//     subtitle: "React Native",
+//     caption: "2 of 12 sections",
+//     logo: require("../assets/logo-react.png")
+//   },
+//   {
+//     title: "Props and Icons",
+//     image: require("../assets/background13.jpg"),
+//     subtitle: "React Native",
+//     caption: "3 of 12 sections",
+//     logo: require("../assets/logo-react.png")
+//   },
+//   {
+//     title: "Static Data and Loop",
+//     image: require("../assets/background14.jpg"),
+//     subtitle: "React Native",
+//     caption: "4 of 12 sections",
+//     logo: require("../assets/logo-react.png")
+//   }
+// ];
 
 const courses = [
   {
